f11cb2d3e8a6f3f1ce87707fe74037fb67ca7876
RomanovRoma <romanian.reget@gmail.com>
2021-01-20 13:28:47 +0300

    feat: add action.payload

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import { createStore } from 'redux'
 const reducer = (state = 0, action) => {
 
   switch (action.type) {
-    case 'INC':
-      return state + 1
+    case "RND":
+      return state + action.payload
 
-      case 'DEC':
-      return state - 1
+    case "INC":
+      return state + 1;
+
+    case "DEC":
+      return state - 1;
 
     default:
-      return state
+      return state;
   }
 }
 
@@ -26,6 +29,13 @@ document
   .getElementById("dec")
   .addEventListener("click", () => {
     store.dispatch({ type: "DEC" });
+  });
+
+document
+  .getElementById("rnd")
+  .addEventListener("click", () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch({ type: "RND", payload });
   });
 
 const update = () => {
